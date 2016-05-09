07d575d3cad085695e21d8dc5aee1c658e116aad
Sawan Gupta <sawan.gupta92@gmail.com>
2016-05-09 16:00:24 +0530

    Fix 2-1 stable

--- app/controllers/spree/admin/invoice_controller.rb
+++ app/controllers/spree/admin/invoice_controller.rb
@@ -3,9 +3,9 @@ module Spree
     class InvoiceController < Spree::BaseController
       def lasku
         template = params[:template]
-        eval "@#{template} = true"
-        @order = Spree::Order.find_by_number(params[:id])
-        render  template , :layout => false
+        instance_variable_set('@' + template, true)
+        @order = Spree::Order.find_by(number: params[:id])
+        render template, layout: false
       end
     end
   end
--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
-                     :name => "add_html_invoice_buttons",
-                     :insert_after => ".sidebar",
-                     :partial => "spree/admin/orders/html_buttons",
-                     :disabled => false)
+Deface::Override.new(
+  virtual_path: "spree/admin/shared/_order_tabs",
+  name: "add_html_invoice_buttons",
+  insert_after: '[data-hook="admin_order_tabs"]',
+  partial: "spree/admin/orders/html_buttons",
+  disabled: false
+)
